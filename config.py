a9041992d812f33127f5f81bb45f86680a22fc9f
Vivek R <vividvilla@gmail.com>
2015-03-18 00:04:42 +0530

    "new update"

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DEBUG = True
 SITE = {
 	'title': 'Your blog title', # Required
 	'description': 'Your blog description', # Required
-	'author': 'Your name', # Required
+	'author': ['Your name', 'another name'], # Required
 	'limit': 10, # Required
 	'custom_home_page' : '', # Enter slug to set any page as homepage (default to list style)
 	'summary_offset': 180, # Optional
@@ -13,5 +13,5 @@ SITE = {
 	'domain_url': 'http://vivek.github.io', # with http or https
 	'github_domain': 'vivek.github.io', # Optional
 	'github_repo': 'https://github.com/vividvilla/vividvilla.github.io.git', # Optional
-	'pygments': '' # set pygments style
+	'pygments': '', # set pygments style
 }
